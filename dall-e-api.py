83cca8d46be5ba01b45831d71d3eca43b442a49b
simonbuusjensen <simonbuusjensen@hotmail.com>
2023-08-15 14:54:13 +0200

    Updated dall-e api

--- dall-e-api.py
+++ dall-e-api.py
@@ -1,20 +1,30 @@
 import openai
 import dotenv
 import requests
+import os
 from PIL import Image
 from io import BytesIO
 
-dotenv.load_dotenv()
-
 # Read the API key from .env file
+dotenv.load_dotenv()
 openai.api_key = dotenv.get_key(".env", "OPENAIKEY")
 data_dir = dotenv.get_key(".env", "DATADIR")
 
-file_name="00000001_005" 
+file_name="00000645_003" 
+
+input_img = f"{data_dir}/masked/{file_name}_stroke_masked.png"
+mask_img = f"{data_dir}/masks_transparent/{file_name}_stroke_mask.png"
+assert os.path.exists(input_img) and os.path.exists(mask_img), f"Input or mask image does not exist: {input_img} {mask_img}"
+
+output_dir = f"{data_dir}/inference"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+output_fn = os.path.join(output_dir, file_name + ".png")
+assert not os.path.exists(output_fn), f"Already performed inference for {file_name}"
 
 response = openai.Image.create_edit(
-  image=open(f"{data_dir}/{file_name}_stroke_masked.png", "rb"),
-  mask=open(f"{data_dir}/masks/{file_name}_stroke_mask.png", "rb"),
+  image=open(input_img, "rb"),
+  mask=open(mask_img, "rb"),
   prompt="A cat",
   n=1,
   size="512x512"
@@ -29,5 +39,4 @@ img = Image.open(BytesIO(response.content))
 
 # Resize to 360x360
 img = img.resize((360, 360), Image.LANCZOS)
-
-img.save(f"{data_dir}/{file_name}.png")
+img.save(output_fn)
