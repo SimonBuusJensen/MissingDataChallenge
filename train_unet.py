a4ebb06daaeb88c4f58655714d742aa7e27cda0f
simonbuusjensen <simonbuusjensen@hotmail.com>
2023-08-15 14:26:59 +0200

    Updated train_unet to load from dummy config

--- train_unet.py
+++ train_unet.py
@@ -1,4 +1,5 @@
 import torch
+import argparse
 from torch import nn
 import torch.nn.functional as F
 
@@ -15,6 +16,7 @@ import cv2
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.nn import InstanceNorm2d, BatchNorm2d
 from unet_test_2 import UNet
+from inpaint_config import InPaintConfig
 
 class InpaintingDataset(Dataset):
     def __init__(self, img_dir, masked_img_dir, mask_dir, txt_file, transform=None):
@@ -59,26 +61,6 @@ class InpaintingDataset(Dataset):
 
         return img, masked_img, mask
 
-
-img_dir = '/home/pabllo/summer_school_ws/src/cats/MissingDataOpenData/originals'
-masked_img_dir = '/home/pabllo/summer_school_ws/src/cats/MissingDataOpenData/masked'
-mask_dir = '/home/pabllo/summer_school_ws/src/cats/MissingDataOpenData/masks'
-train_txt_file = '/home/pabllo/summer_school_ws/src/cats/MissingDataOpenData/data_splits/training.txt'
-val_txt_file = '/home/pabllo/summer_school_ws/src/cats/MissingDataOpenData/data_splits/validation_200.txt'
-
-transform = transforms.Compose([
-    transforms.ToTensor()
-])
-
-# Create the datasets
-train_dataset = InpaintingDataset(img_dir, masked_img_dir, mask_dir, train_txt_file, transform=transform)
-val_dataset = InpaintingDataset(img_dir, masked_img_dir, mask_dir, val_txt_file, transform=transform)
-
-train_batch_size = 10
-# Create the dataloaders
-train_dataloader = DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True, num_workers=4)
-val_dataloader = DataLoader(val_dataset, batch_size=train_batch_size, shuffle=False, num_workers=4)
-
 class WeightedPerceptualLoss(nn.Module):
     def __init__(self):
         super(WeightedPerceptualLoss, self).__init__()
@@ -114,101 +96,127 @@ def weighted_mse_loss(input, target, mask, weight=10):
 
     return torch.mean(weighted_mse)
 
-# Training loop
-num_epochs = 12
+if __name__ == "__main__":
 
-n_total_steps = len(train_dataloader)
-validate_every_n_steps = 50
-# Placeholder for the best validation loss
-best_val_loss = np.inf
+    args = argparse.ArgumentParser(description='InpaintImages')
+    config = InPaintConfig(args)
+    settings = config.settings
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    data_dir = settings["dirs"]["input_data_dir"]
+    img_dir = os.path.join(data_dir, "originals")
+    masked_img_dir = os.path.join(data_dir, "masked") 
+    mask_dir = os.path.join(data_dir, "masks")
+    train_txt_file = os.path.join(data_dir, "data_splits", "training.txt")
+    val_txt_file = os.path.join(data_dir, "data_splits", "validation_200.txt")
 
-model = UNet().to(device)
-model.load_state_dict(torch.load("/home/pabllo/summer_school_ws/src/MissingDataChallenge/weighed_model_3.ckpt"))
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
 
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-#perceptual_loss = PerceptualLoss().to(device)
+    # Create the datasets
+    train_dataset = InpaintingDataset(img_dir, masked_img_dir, mask_dir, train_txt_file, transform=transform)
+    val_dataset = InpaintingDataset(img_dir, masked_img_dir, mask_dir, val_txt_file, transform=transform)
 
-weighted_perceptual_loss = WeightedPerceptualLoss().to(device)
+    train_batch_size = 10
+    # Create the dataloaders
+    train_dataloader = DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True, num_workers=4)
+    val_dataloader = DataLoader(val_dataset, batch_size=train_batch_size, shuffle=False, num_workers=4)
 
-scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=5, verbose=True)
+    # Training loop
+    num_epochs = 12
 
-insert_image = cv2.imread("/home/pabllo/summer_school_ws/src/MissingDataChallenge/trained_model/average_image.png")
-tensor_image = torch.tensor(insert_image/255, dtype=torch.float32).permute(2, 0, 1)
+    n_total_steps = len(train_dataloader)
+    validate_every_n_steps = 50
+    # Placeholder for the best validation loss
+    best_val_loss = np.inf
 
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-for epoch in range(num_epochs):
-    for i, (img, masked_img, mask) in enumerate(train_dataloader):
+    model = UNet().to(device)
+    # model.load_state_dict(torch.load(f"{data_dir}/weighed_model_3.ckpt"))
 
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    #perceptual_loss = PerceptualLoss().to(device)
 
-        batch_size = masked_img.shape[0]
-        tensor_image_batch = tensor_image.repeat(batch_size, 1, 1, 1)
-        expanded_mask = mask.expand_as(masked_img)
-        masked_img[expanded_mask == 1] = tensor_image_batch[expanded_mask == 1]
+    weighted_perceptual_loss = WeightedPerceptualLoss().to(device)
 
-        """
-        img_np = masked_img[0].cpu().numpy()
-        img_np = img_np.transpose(1, 2, 0)
-        img_np = (img_np * 255).astype(np.uint8)
-
-        cv2.imshow('Image', img_np)
-        cv2.waitKey(0)
-        """
-
-        # Move the data to the GPU
-        img, masked_img, mask = img.to(device), masked_img.to(device), mask.to(device)
-
-        model.train()
-        optimizer.zero_grad()
-
-
-        output = model(masked_img, mask)
-
-        weighted_mse = weighted_mse_loss(output,img,mask)
-
-        weighted_percept = weighted_perceptual_loss(output,img,mask)
+    scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=5, verbose=True)
+    
+    insert_image = cv2.imread(f"{data_dir}/trained_model/average_image.png")
+    tensor_image = torch.tensor(insert_image/255, dtype=torch.float32).permute(2, 0, 1)
 
 
-        total_loss = weighted_mse + weighted_percept
-
-        total_loss.backward()
-        optimizer.step()
-
-        print(f"Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Training Loss: {total_loss.item()}, MSE: {weighted_mse.item()}, WeightedPerceptualLoss: {weighted_percept.item()}")
-
-        # Validate the model every validate_every_n_steps steps
-        if (i + 1) % validate_every_n_steps == 0:
-            model.eval()
-            with torch.no_grad():
-                val_losses = []
-                for img, masked_img, mask in val_dataloader:
+    for epoch in range(num_epochs):
+        for i, (img, masked_img, mask) in enumerate(train_dataloader):
 
 
-                    batch_size = masked_img.shape[0]
-                    tensor_image_batch = tensor_image.repeat(batch_size, 1, 1, 1)
-                    expanded_mask = mask.expand_as(masked_img)
-                    masked_img[expanded_mask == 1] = tensor_image_batch[expanded_mask == 1]
-                    # Move the data to the GPU
-                    img, masked_img, mask = img.to(device), masked_img.to(device), mask.to(device)
+            batch_size = masked_img.shape[0]
+            tensor_image_batch = tensor_image.repeat(batch_size, 1, 1, 1)
+            expanded_mask = mask.expand_as(masked_img)
+            masked_img[expanded_mask == 1] = tensor_image_batch[expanded_mask == 1]
+
+            """
+            img_np = masked_img[0].cpu().numpy()
+            img_np = img_np.transpose(1, 2, 0)
+            img_np = (img_np * 255).astype(np.uint8)
+
+            cv2.imshow('Image', img_np)
+            cv2.waitKey(0)
+            """
+
+            # Move the data to the GPU
+            img, masked_img, mask = img.to(device), masked_img.to(device), mask.to(device)
+
+            model.train()
+            optimizer.zero_grad()
 
 
-                    output = model(masked_img, mask)
+            output = model(masked_img, mask)
 
-                    weighted_mse = weighted_mse_loss(output,img,mask)
+            weighted_mse = weighted_mse_loss(output,img,mask)
 
-                    weighted_percept = weighted_perceptual_loss(output,img,mask)
+            weighted_percept = weighted_perceptual_loss(output,img,mask)
 
-                    # Calculate total loss
-                    total_loss = weighted_mse + weighted_percept
-                    val_losses.append(total_loss.item())
 
-                avg_val_loss = np.mean(val_losses)
-                scheduler.step(avg_val_loss)
-                print(f"Step [{i+1}/{n_total_steps}], Avg Validation Loss: {avg_val_loss}")
+            total_loss = weighted_mse + weighted_percept
 
-                # Checkpointing
-                if avg_val_loss < best_val_loss:
-                    best_val_loss = avg_val_loss
-                    print(f"Improved validation loss {best_val_loss}: Saving model ...")
-                    torch.save(model.state_dict(), 'weighed_model_3.ckpt')
+            total_loss.backward()
+            optimizer.step()
+
+            print(f"Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Training Loss: {total_loss.item()}, MSE: {weighted_mse.item()}, WeightedPerceptualLoss: {weighted_percept.item()}")
+
+            # Validate the model every validate_every_n_steps steps
+            if (i + 1) % validate_every_n_steps == 0:
+                model.eval()
+                with torch.no_grad():
+                    val_losses = []
+                    for img, masked_img, mask in val_dataloader:
+
+
+                        batch_size = masked_img.shape[0]
+                        tensor_image_batch = tensor_image.repeat(batch_size, 1, 1, 1)
+                        expanded_mask = mask.expand_as(masked_img)
+                        masked_img[expanded_mask == 1] = tensor_image_batch[expanded_mask == 1]
+                        # Move the data to the GPU
+                        img, masked_img, mask = img.to(device), masked_img.to(device), mask.to(device)
+
+
+                        output = model(masked_img, mask)
+
+                        weighted_mse = weighted_mse_loss(output,img,mask)
+
+                        weighted_percept = weighted_perceptual_loss(output,img,mask)
+
+                        # Calculate total loss
+                        total_loss = weighted_mse + weighted_percept
+                        val_losses.append(total_loss.item())
+
+                    avg_val_loss = np.mean(val_losses)
+                    scheduler.step(avg_val_loss)
+                    print(f"Step [{i+1}/{n_total_steps}], Avg Validation Loss: {avg_val_loss}")
+
+                    # Checkpointing
+                    if avg_val_loss < best_val_loss:
+                        best_val_loss = avg_val_loss
+                        print(f"Improved validation loss {best_val_loss}: Saving model ...")
+                        torch.save(model.state_dict(), os.path.join(data_dir, 'weighed_model_3.ckpt'))
